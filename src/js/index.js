ab9b74c710654409b052d3439664fe2f16bd4b1e
morbi <the_morbidus@hotmail.com>
2020-02-18 16:14:24 -0500

    Created Search class for querying api requests

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = '2815623487516924351942351465';
-  try {
-    const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
+const search = new Search('pizza');
 
-getResults('pizza');
+console.log(search);
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,3 +1,20 @@
-const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try {
+      const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
 
 // for recipe.js  const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
